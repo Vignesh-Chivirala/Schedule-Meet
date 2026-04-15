a13d5aff21859d814289738a8eced6a08dcd831f
Vignesh <collegevignesh28@gmail.com>
2026-04-15 13:12:48 +0530

    connecting to cloud services of MYSQL

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,11 @@
 import mysql from "mysql2";
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "chvignesh28",
-  database: "schedule_app",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 function ensureSchema() {
