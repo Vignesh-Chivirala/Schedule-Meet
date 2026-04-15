6d80d668db0a90373aac963b0c542f61ea1ea321
Vignesh <collegevignesh28@gmail.com>
2026-04-15 21:59:01 +0530

    vercel changes

--- backend/server.js
+++ backend/server.js
@@ -14,11 +14,15 @@ app.use(cors({
 }));
 app.use(express.json());
 
+
+
 app.use("/api/events", eventRoutes);
 app.use("/api/bookings", bookingRoutes);
 app.use("/api/availability", availabilityRoutes);
 
-
+app.get("/", (req, res) => {
+  res.send("🚀 API is running");
+});
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");
