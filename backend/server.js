2a07af72e5d3655dfe0c561cfd9deb40f8653c1e
Vignesh <collegevignesh28@gmail.com>
2026-04-15 20:27:54 +0530

    cors updated

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,10 @@ import availabilityRoutes from "./routes/availabilityRoutes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: true, 
+  credentials: true,
+}));
 app.use(express.json());
 
 app.use("/api/events", eventRoutes);
